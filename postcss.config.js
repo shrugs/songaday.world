0f9896000c3558b18a3e5c894b7d947d588eda79
Matt Condon (shrugs) <mattgcondon@gmail.com>
2020-01-15 16:48:45 -0600

    deps: upgrade to next.js 9.2

--- postcss.config.js
+++ postcss.config.js
@@ -1,9 +1,7 @@
-/* eslint-disable @typescript-eslint/no-var-requires */
-
 module.exports = {
-  plugins: [
-    require('postcss-import'),
-    require('tailwindcss'),
-    require('postcss-preset-env')({ stage: 2 }),
-  ],
+  plugins: {
+    'postcss-import': {},
+    tailwindcss: {},
+    'postcss-preset-env': { stage: 2 },
+  },
 };
