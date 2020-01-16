eb3187056e925589fc894bffc971e688bc97c9e7
Matt Condon (shrugs) <mattgcondon@gmail.com>
2020-01-16 11:39:43 -0600

    feat: add purgecss

--- postcss.config.js
+++ postcss.config.js
@@ -2,6 +2,14 @@ module.exports = {
   plugins: {
     'postcss-import': {},
     tailwindcss: {},
+    ...(process.env.NODE_ENV === 'production'
+      ? {
+          '@fullhuman/postcss-purgecss': {
+            content: ['./pages/**/*.{js,jsx,ts,tsx}', './components/**/*.{js,jsx,ts,tsx}'],
+            defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || [],
+          },
+        }
+      : {}),
     'postcss-preset-env': { stage: 2 },
   },
 };
