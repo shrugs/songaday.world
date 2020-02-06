4e77c0e3c6588b269fe4a545bfa914514b3736b6
Matt Condon (shrugs) <mattgcondon@gmail.com>
2020-02-05 18:09:49 -0600

    feat: add link to homepage in about navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -19,6 +19,14 @@ function Navbar() {
       </Link>
       <div className="ml-2 flex-auto flex flex-row justify-end items-center">
         <a
+          className="font-normal text-sm underline text-gray-700 mr-4"
+          href="https://www.jonathanmann.net/"
+          rel="noopener noreferrer"
+          target="_blank"
+        >
+          About Song a Day
+        </a>
+        <a
           className="font-normal text-sm underline text-gray-700"
           href="https://twitter.com/songadaymann"
           rel="noopener noreferrer"
